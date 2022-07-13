6f55442924fa56ccb690849ea9af5b20d294b2eb
alicyja <alicja.godyn@hotmail.com>
2022-07-13 15:33:57 +0200

    Praca domowa A Godyń

---
+++ src/main/java/pl/cyber/trainees/Calculator.java
@@ -0,0 +1,31 @@
+package pl.cyber.trainees;
+
+public class Calculator {
+    public static void main(String[] args) {
+        wydrukujWszystkieOperacje(5,6);
+        wydrukujWszystkieOperacje(3,8);
+        wydrukujWszystkieOperacje(2,3);
+        wydrukujWszystkieOperacje(4,2);
+    }
+
+    public static int dodaj(int a, int b) {
+        return a + b;
+    }
+
+    public static int odejmij(int a, int b) {
+        return a - b;
+    }
+
+    public static int pomnoz(int a, int b) {
+        return a * b;
+    }
+    public static double podziel(int a,int b){
+        return (double) a / b;
+    }
+    public static void wydrukujWszystkieOperacje(int a, int b) {
+        System.out.println(a + " + " + b + " = " + dodaj(a, b));
+        System.out.println(a + " - " + b + " = " + odejmij(a, b));
+        System.out.println(a + " * " + b + " = " + pomnoz(a, b));
+        System.out.println(a + " / " + b + " = " + podziel(a, b));
+    }
+}
